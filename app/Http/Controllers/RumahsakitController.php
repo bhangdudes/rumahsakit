6454d75fc1277914d430271c82e6bd716d45594d
cahyes <33574547+cahyes@users.noreply.github.com>
2023-07-09 13:48:25 +0700

    tambah edit

--- app/Http/Controllers/RumahsakitController.php
+++ app/Http/Controllers/RumahsakitController.php
@@ -54,4 +54,25 @@ class RumahsakitController extends Controller
     DB::table('pasien')->where('id',$id)->delete();
     return redirect('/pasien');
   }
+
+    //funtion edit data
+  public function edit($id)
+  {
+    //Untuk mengambil data dari database berdasarkan id
+    $pasien = DB::table('pasien')->where('id', $id)->get();
+    //Akses halaman edit dan mengirim data buku sesuai id
+    return view('pasien/editpasien', ['pasien' => $pasien]);
+  }
+
+  //function update (data setelah diedit)
+  public function update(Request $request)
+  {
+    DB::table('pasien')->where('id', $request->id)->update([
+      'id' => $request->id,
+      'nama_pasien' => $request->nama_pasien,
+      'umur_pasien' => $request->umur_pasien,
+      'no_kamar' => $request->no_kamar,
+    ]);
+    return redirect('/pasien');
+  }
 }
