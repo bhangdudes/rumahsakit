fc4fb5c909c51df86aeca4c0843780f410f7bf53
TeddyWintyas <211226011@mhs.stiki.ac.id>
2023-07-21 22:03:27 +0700

    newloginview

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,117 @@
-<div class="row justify-content-center"style="text center";>
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title text-center">Login</h5>
-            <h6 class="card-subtitle mb-2 text-center text-muted">Isi dengan lengkap</h6>
-            <form action="/authenticate" method="post" class="user">
+<html>
+<head>
+</head>
+<style>
+
+* {
+    padding: 0px;
+    margin: 0px;
+}
+
+header {
+    background-color: black;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 15vh;
+    box-shadow: 5px 5px 10px rgb(0,0,0,0.3);
+}
+h1 {
+    font-family: 'system-ui';
+    text-align: center;
+    letter-spacing: 3px;
+}
+main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 75vh;
+    width: 100%;
+    background: url(home.jpg) no-repeat center center;
+    background-size: cover;
+}
+.form_class {
+    width: 500px;
+    padding: 40px;
+    border-radius: 8px;
+    background-color: white;
+    font-family: 'system-ui';
+    box-shadow: 5px 5px 10px rgb(0,0,0,0.3);
+}
+.form_div {
+    text-transform: uppercase;
+}
+.form_div > label {
+    letter-spacing: 3px;
+    font-size: 1rem;
+}
+.field_class {
+    width: 100%;
+    border-radius: 6px;
+    border-style: solid;
+    border-width: 1px;
+    padding: 5px 0px;
+    text-indent: 6px;
+    margin-top: 10px;
+    margin-bottom: 20px;
+    font-family: 'system-ui';
+    font-size: 0.9rem;
+    letter-spacing: 2px;
+}
+.submit_class {
+    border-style: none;
+    border-radius: 5px;
+    background-color: #FFE6D4;
+    padding: 8px 20px;
+    font-family: 'system-ui';
+    text-transform: uppercase;
+    letter-spacing: .8px;
+    display: block;
+    margin: auto;
+    margin-top: 10px;
+    box-shadow: 2px 2px 5px rgb(0,0,0,0.2);
+    cursor: pointer;
+}
+footer {
+    height: 10vh;
+    background-color: black;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: -5px -5px 10px rgb(0,0,0,0.3);
+}
+footer > p {
+    text-align: center;
+    font-family: 'system-ui';
+    letter-spacing: 3px;
+}
+footer > p > a {
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+}
+</style>
+<body>
+    <header>
+        <h1>SELAMAT DATANG</h1>
+    </header>
+    <main>
+            <form class="form_class" action="/authenticate" method="post" class="user">
+                <div class="form_div">
                 @csrf
-                <div class="form-group mt-3">
-                   <input type="text" name="nama" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Masukkan nama anda" value="{{ old('nama') }}">
-                </div>
-                <div class="form-group mt-3">
-             	 <input type="password" name="password" class="form-control" id="exampleInputPassword" placeholder="Password" value="{{ old('password') }}">
-                </div>
-               
-                <div class="form-group mt-3 text-center">
-     			  <button type="submit" class="btn btn-primary btn-block">Login</button>
+                <div class="form_div">
+                <label>Username:</label>
+                <input class="field_class" input type="text" name="nama">
+                <label>Password:</label>
+                <input class="field_class" type="password" name="password">
+                <button class="submit_class" type="submit">LOGIN</button>
                 </div>
             </form>
-        </div>
-    </div>
-</div>
+    </main>
+    <footer>
+        <p>Developed By Penghuni Terakhir</a></p>
+    </footer>
+</body>
+</html>
